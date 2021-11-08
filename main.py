445aa212e6e31dbde2f7759fa301ac06a90ebe8c
garyBigdata <91714392+garyBigdata@users.noreply.github.com>
2021-11-08 17:55:41 +0900

    211108_update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 import pymysql
+import simplejson
 
 app = Flask(__name__)
 
@@ -61,6 +62,52 @@ def chart():
     finally:
         sample_db.close()
     return render_template('dash.html', result=result, labels=labels, cost=cost, tax=tax)
+
+@app.route('/pasing')
+def pasing():
+    a = []
+    b =[]
+    try:
+        sample_db = pymysql.connect(
+            user='root',
+            passwd='1234',
+            host='localhost',
+            db='web_test'
+        )
+        cursor = sample_db.cursor()
+        sql = "SELECT occyp_type, COUNT(occyp_type) AS cnt FROM train group by occyp_type;"
+        cursor.execute(sql)
+        result=cursor.fetchall()
+        for list in result:
+            if list[0]:
+                b.append(list[0])
+                a.append(list[1])
+            else:
+                b.append('empty')
+                a.append(list[1])
+        print(b)
+    finally:
+        sample_db.close()
+    return render_template('pasing.html', result=result, a = a, b=b)
+
+@app.route('/scatter')
+def scatter():
+    try:
+        sample_db = pymysql.connect(
+            user='root',
+            passwd='1234',
+            host='localhost',
+            db='web_test'
+        )
+        cursor = sample_db.cursor()
+        sql = "SELECT abs(DAYS_BIRTH)/365, income_total  FROM train;"
+        cursor.execute(sql)
+        result2 = cursor.fetchall();
+        result = simplejson.dumps(result2)
+        print(result2)
+    finally:
+        sample_db.close()
+    return render_template('scatter.html', result=result, result2=result2)
 
 app.run
 
