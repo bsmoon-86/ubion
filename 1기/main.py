6ce8aba397cd534aea367ebec9eb40c40120a869
moonbs86 <ekflafg@gmail.com>
2022-02-25 17:00:41 +0900

    220225 update

--- 1기/main.py
+++ 1기/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, make_response
 import pymysql
 import json
 import simplejson
@@ -15,7 +15,7 @@ def index():
             user='root',
             passwd='1234',
             host='localhost',
-            db='web_test'
+            db='test'
         )
         cursor = sample_db.cursor()
         sql = "select * from chart"
@@ -26,15 +26,35 @@ def index():
             labels.append(list[0])
             cost.append(list[1])
             tax.append(list[2])
-        print(result)
     finally:
         sample_db.close()
 
+    try:
+        sample_db = pymysql.connect(
+            user='root',
+            passwd='1234',
+            host='localhost',
+            db='test'
+        )
+        cursor = sample_db.cursor()
+        sql = "select * from chart limit 1"
+        cursor.execute(sql) 
+        # sql구문이 실행
+        result2=cursor.fetchall()
+    finally:
+        sample_db.close()
+  
+    print(result)      
+    print(result2)
     return render_template('index.html', result=result, label=labels, data1=cost, data2=tax)
 
 @app.route('/dy')
 def dy():
-    return render_template('dygragh.html')
+    a = {
+        "name" : "moon",
+        "age" : "37"
+        }
+    return a
 
 @app.route('/chart')
 def chart():
@@ -45,20 +65,22 @@ def chart():
             user='root',
             passwd='1234',
             host='localhost',
-            db='web_test'
+            db='test'
         )
         cursor = sample_db.cursor()
-        sql = "SELECT abs(DAYS_BIRTH)/365, income_total FROM train limit 30;"
+        sql = "SELECT abs(DAYS_BIRTH) as x, income_total as y FROM train limit 30;"
         cursor.execute(sql)
-        result2 = cursor.fetchall()
-        result = simplejson.dumps(result2)
-        for list in result2:
-            gragh_data[i]= {"x": list[0], "y": list[1]}
-            i+1
-            print(gragh_data)
+        # result2 = cursor.fetchall()
+        # result = json.dumps(result2)
+        r = [dict((cursor.description[i][0], value)                
+        for i, value in enumerate(row)) 
+        for row in cursor.fetchall()]
     finally:
         sample_db.close()
-    return render_template('dash.html', result=gragh_data, result2=result2)
+    result = json.dumps(r, ensure_ascii=False)
+    res = make_response(result)
+    print(result)
+    return render_template('dash_.html', result=result)
 
 @app.route('/pasing')
 def pasing():
@@ -94,14 +116,17 @@ def scatter():
             user='root',
             passwd='1234',
             host='localhost',
-            db='web_test'
+            db='test'
         )
         cursor = sample_db.cursor()
         sql = "SELECT abs(DAYS_BIRTH)/365, income_total  FROM train;"
         cursor.execute(sql)
         result2 = cursor.fetchall();
         result = simplejson.dumps(result2)
-        print(result2)
+        r = [dict((cursor.description[i][0], value)                
+        for i, value in enumerate(row)) 
+        for row in cursor.fetchall()]
+        print(r)
     finally:
         sample_db.close()
     return render_template('scatter.html', result=result, result2=result2)
@@ -142,12 +167,12 @@ def get_json():
         cursor = sample_db.cursor()
         sql = "select * from test where No = %s"
         val = (_message)
-        sql2 = "select * from test"
         cursor.execute(sql, val)
         result=cursor.fetchall()
-        cursor.execute(sql2)
-        result2 = cursor.fetchall()
-        result.append(result2)
+        list2 = [];
+        for list in result:
+            list[0].remove
+        
         # sql구문이 실행
     finally:
         sample_db.close()
